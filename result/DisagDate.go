65a9640c7765bb16824b4c0c45991010058255f1
DBX12 <dbx12@dbx12.de>
2024-10-30 19:58:44 +0100

    Restructure DisagDate for easier testing

--- result/DisagDate.go
+++ result/DisagDate.go
@@ -8,7 +8,12 @@ import (
 type DisagDateTime time.Time
 
 func (d *DisagDateTime) UnmarshalXMLAttr(attr xml.Attr) error {
-	return d.parseString(attr.Value)
+	v, err := d.parseString(attr.Value)
+	if err != nil {
+		return err
+	}
+	*d = DisagDateTime(*v)
+	return nil
 }
 
 func (d *DisagDateTime) UnmarshalXML(dec *xml.Decoder, start xml.StartElement) error {
@@ -19,14 +24,24 @@ func (d *DisagDateTime) UnmarshalXML(dec *xml.Decoder, start xml.StartElement) e
 	if err != nil {
 		return err
 	}
-	return d.parseString(str)
-}
 
-func (d *DisagDateTime) parseString(str string) error {
-	t, err := time.Parse("02.01.2006 15:04:05", str)
+	v, err := d.parseString(str)
 	if err != nil {
 		return err
 	}
-	*d = DisagDateTime(t)
+
+	*d = DisagDateTime(*v)
 	return nil
+}
+
+func (d *DisagDateTime) parseString(str string) (*time.Time, error) {
+	if str == "" {
+		return &time.Time{}, nil
+	}
+
+	t, err := time.Parse("02.01.2006 15:04:05", str)
+	if err != nil {
+		return nil, err
+	}
+	return &t, nil
 }
